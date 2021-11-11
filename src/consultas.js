0282dcdb53284b9ccabe290ebd3e8daa0de23dfa
SibariEmber <sibariember@gmail.com>
2021-11-11 20:53:33 +0100

    Versión 1.1

--- src/consultas.js
+++ src/consultas.js
@@ -1,6 +1,6 @@
 db.peces.find(
     {tipoAgua: "dulce"} 
-).pretty()
+)
 
 
 
--- src/inserciones.js
+++ src/inserciones.js
@@ -2,110 +2,121 @@ db.peces.insertOne(
     {
         nombre: "Pez Betta",
         especie:"Betta splendens",
-        razas: ["Cola de velo","Plakat","Dumbo","Media Luna","Súper Delta","Corona","Cola de rosa"],
-        reproducción:"Ovíparo",
-        tamañoAdulto:[6,7],
-        tamañoMinimoAcuario:10,
-        colores:["Verde","Marrón","Gris","Azul","Rojo","Blanco"],
-        temperaturaOptima:[25,26,27,28],
         alimentación:"Omnívora",
-        tipoAgua:"Dulce" ,
-        pH:7,
+        reproducción:"Ovíparo",
+        esperanzaVida:[2,3],
+        tamaño:[6,7],
         comportamiento:["Agresivo","Territorial"],
+        cardumen: false,
         procedencia:["Tailandia","Camboya"],
-        esperanzaVida:[2-3]
+        agua:"Dulce",
+        temperatura:[25,26,27,28],
+        pH:7,
+        volumenAcuario:10,    
     }
 )
 
 db.peces.insertMany(
     [
         {  
-            nombre:"Pez Guppy", especie: "Poecilia Reticulata",
-            tamañoAdulto:[3,4,5,6,7,8,9],razas: ["Cola de pala","Cola de espada","Cola de doble espada","Cola de Lira","Cola redonda","Cola de aguja","Cola de punta","Cola triangular","Cola de abanico","Cola de bandera","Cola de velo"],
-            reproducción:"Ovovíparo", tamañoMinimoAcuario:3 ,colores:[] ,temperaturaOptima:27 ,alimentación:"Omnívora" ,
-            tipoAgua:["Dura","Alcalina"] ,pH:7.3, comportamiento:"Pacífico" ,procedencia:["Venezuela", "Brasil" , "Trinidad" , "Bárbados"] ,
-            esperanzaVida:2
+            nombre:"Pez Guppy", especie: "Poecilia Reticulata", tamaño:[3,4,5,6,7,8,9],reproducción:"Ovovíparo", volumenAcuario:3,  temperatura:27 , alimentación:"Omnívora" ,
+            agua:["Dulce","Cálida","Alcalina"] ,pH:7.3, comportamiento:"Pacífico" ,procedencia:["Venezuela", "Brasil" , "Trinidad" , "Bárbados"],cardumen: false ,esperanzaVida:2
         },
 
         {
-            nombre:"Pez Tetra Neón", especie: "Paracheirodon innesi", tamañoAdulto:[2,3,4], razas: ["Black Diamond","Dorado","Albino","Cabeza de diamante","Blanco Brillante","Velo"],
-            reproducción: "Ovíparos", tamañoMinimoAcuario:60 ,colores:"Azul metálico , Rojo y Plateado",temperaturaOptima:[20,21,22,23,24,25],
-            alimentación:"Omnívora", tipoAgua:["Ácida","Blanda"] , pH:[5,6,7], comportamiento:"",
-            procedencia:["Amazonas","Perú","Colombia","Cardumen"], cardumen: 10, esperanzaVida:[5-8]
+            nombre:"Pez Tetra Neón", especie: "Paracheirodon innesi", tamaño:[2,3,4], reproducción: "Ovíparos", volumenAcuario:60 ,temperatura:[20,21,22,23,24,25], alimentación:"Omnívora",
+            agua:["Dulce","Cálida","Ácida","Blanda"] , pH:[5,6,7], comportamiento:["Tímido","Pacífico"], procedencia:["Amazonas","Perú","Colombia"], cardumen: true, esperanzaVida:[5,6,7,8]
         },
 
         {
-            nombre:"Pez Platy", especie: "Xiphophorus maculatus", tamañoAdulto:[5,6,7], razas: ["Única"], reproducción: "Ovovivíparos",
-            tamañoMinimoAcuario:60 , colores:["Negro","Oro","Verde","Rojo","Marrón","Azul"], temperaturaOptima:[25,26],
-            alimentación:"Omnívoro", tipoAgua:["Dulce","Caliente"] , pH:[7,8.2], comportamiento:["Pacífico","Tímido","Grupo de 4"],
-            procedencia:["Guatemala","Honduras","México"], esperanzaVida:[2-3]
+            nombre:"Pez Platy", especie: "Xiphophorus maculatus", tamaño:[5,6,7],reproducción: "Ovovivíparos", volumenAcuario:60 ,temperatura:[25,26], alimentación:"Omnívora",
+            agua:["Dulce","Cálida"] , pH:[7,8.2], comportamiento:["Pacífico","Tímido","Grupo de 4"], procedencia:["Guatemala","Honduras","México"], cardumen:false, esperanzaVida:[2,3]
         },
 
         {
-            nombre:"Pez Molly", especie: "Poecilia velífera", tamañoAdulto:[15-18], razas: ["Única"], reproducción: "Ovovípara",
-            tamañoMinimoAcuario: 120 ,colores:["Verde azulado","Albino","Dorado","Leopardo","Negro","Blanco"], temperaturaOptima:[26-28],
-            alimentación:"Omnívora", tipoAgua:["Dura","Alcalina"] , pH:[7-8], comportamiento:["Pacífico","Activo"], procedencia:["México"],
-            esperanzaVida:[2-3]
+            nombre:"Pez Molly", especie: "Poecilia velífera", tamaño:[8,9,10,11,12], reproducción: "Ovovívípara", volumenAcuario: 120 , temperatura:[26,28], alimentación:"Omnívora",
+            agua:["Dulce","Cálida","Dura","Alcalina"] , pH:[7,8], comportamiento:["Pacífico","Activo"], procedencia:["México"], cardumen:false, esperanzaVida:[2,3]
         },
 
         {
-            nombre:"Pez Borrachito", especie: "Hemigrammus rhodostomus", tamañoAdulto:[4-5], razas: [""], reproducción: "Ovípara",
-            tamañoMinimoAcuario:70  , colores:"Cuerpo plateado , cabeza roja y aletas con manchas negra y blancas",temperaturaOptima:[24-27],
-            alimentación:"", tipoAgua:["Ácida","Blanda"] , pH:[5.5-7], comportamiento:["Activo","Pacífico"],procedencia:["Amazonas","Río Orinoco","Cardumen"],
-            cardumen:8, esperanzaVida:4
+            nombre:"Pez Borrachito", especie: "Hemigrammus rhodostomus", tamaño:[4,5], reproducción: "Ovípara", volumenAcuario:70,temperatura:[24,25,26,27], alimentación:"Omnívora", 
+            agua:["Dulce","Cálida","Ácida","Blanda"] , pH:[5,6,7], comportamiento:["Activo","Pacífico"],procedencia:["Amazonas","Río Orinoco",], cardumen:true, esperanzaVida:4
         },
 
         {
-            nombre:"Pez cebrita", especie: "Danio rerio", tamañoAdulto:[5-6], razas: "", reproducción: "Ovípara", tamañoMinimoAcuario:  ,
-            colores:"Blanco amarillento", temperaturaOptima:[20-26], alimentación:"Omnívora", tipoAgua:["Blanda","Oxigenada"] ,
-            pH:[6.5-7.5], comportamiento:["Activo" , "Veloz" , "Resistente", "Cardumen"], procedencia:["India oriental","Bangladesh"],
-            cardumen: 6, esperanzaVida:[3-4]
+            nombre:"Pez Cebrita", especie: "Danio rerio", tamaño:[5,6], reproducción: "Ovípara", volumenAcuario:80, temperatura:[20,21,22,23,24,25,26], alimentación:"Omnívora",
+            agua:["Dulce","Cálida","Blanda","Oxigenada"] , pH:[6.5,7,7.5], comportamiento:["Activo" , "Veloz" , "Resistente"], procedencia:["India oriental","Bangladesh"], cardumen: true, esperanzaVida:[3,4]
         },
 
         {
-            nombre:"Pez ángel", especie: "Pterophyllum scalare", tamañoAdulto:[12-18], razas: ["Scalare","Altum","Leopoldi"],
-            reproducción: ["Ovípara"], tamañoMinimoAcuario:60  , colores:["Naranja","Albino"], temperaturaOptima:[24-30],
-            alimentación:"Omnívora", tipoAgua:["Dulce","Templada"] , pH:[6-8], comportamiento:["Pacífico","Social"],
-            procedencia:["Perú","Colombia","Brasil"], esperanzaVida:[5-8]
+            nombre:"Pez Ángel", especie: "Pterophyllum scalare", tamaño:[12,13,14,15,16,17,18], reproducción: ["Ovípara"], volumenAcuario:60, temperatura:[24,30], alimentación:"Omnívora", 
+            agua:["Dulce","Cálida","Templada"] , pH:[6,7,8], comportamiento:["Pacífico","Social"], procedencia:["Perú","Colombia","Brasil"], cardumen:false,esperanzaVida:[5,6,7,8]
         },
 
         {
-            nombre:"Pez plecostomus", especie: "Hypostomus plecostomus", tamañoAdulto:[30-60], reproducción: "DIFICIL DE SEXAR",
-            tamañoMinimoAcuario:[300-500], colores:["Marrón","Negro"], temperaturaOptima:[22-30],alimentación:"Omnívoro",
-            tipoAgua:[""] , pH:[6-8], comportamiento:["Pacífico"], procedencia:["Río Orinoco","Costa Rica","Uruguay","Panamá","Colombia","Venezuela"],
-            esperanzaVida:[15]
+            nombre:"Pez Plecostomus", especie: "Hypostomus plecostomus", tamaño:[30,40,50,60], reproducción: "Ovíparo", volumenAcuario:[300-500], temperatura:[22-30],alimentación:"Omnívora",
+            agua:["Dulce","Cálida","Fría"] , pH:[6-8], comportamiento:["Pacífico"], procedencia:["Río Orinoco","Costa Rica","Uruguay","Panamá","Colombia","Venezuela"],cardumen:false, esperanzaVida:[15]
         },
 
         {
-            nombre:"Pez Disco de Heckel", especie:"Symphysodon", tamañoAdulto:[15-30], razas: ["Discus","Aequifasciatus"],
-            reproducción: "Ovípara", tamañoMinimoAcuario: 100, colores:["Amarillo","Azul","Blanco","Rojo","Turquesa"],
-            temperaturaOptima:[28,29], alimentación:"Omnívora", tipoAgua:["Ácida"] , pH:[5.5-6.5], comportamiento:["Tranquilo","Sensible"],
-            procedencia:["Amazonas"], esperanzaVida:[12-14]
+            nombre:"Pez Disco", especie:"Symphysodon", tamaño:[15,20,25,30], reproducción: "Ovípara", volumenAcuario: 100,  temperatura:[28,29], alimentación:"Omnívora", 
+            agua:["Dulce","Cálida","Ácida"] , pH:[5.5,6.5], comportamiento:["Tranquilo","Sensible"], procedencia:["Amazonas"], cardumen:false,esperanzaVida:[12,13,14]
         }
 
         {
-            nombre:"Pez Goldfish", especie:"Carassius auratus", tamañoAdulto:[], razas: ["Telescopio","Shubunkin","Ojos de burbuja"],
-            reproducción:"Ovípara", tamañoMinimoAcuario:40 , colores:["Rojo","Azul"],
-            temperaturaOptima:[16-20], alimentación:"Omnívora", tipoAgua:["Fría"] , pH:[7-7.5], comportamiento:["Tranquilo","Pacífico"],
-            procedencia:["China"], esperanzaVida:[10-25]
+            nombre:"Pez Goldfish", especie:"Carassius auratus", tamaño:[], reproducción:"Ovípara", volumenAcuario:40 , temperatura:[16,17,18,19,20], alimentación:"Omnívora", 
+            agua:["Dulce","Fría"] , pH:[7-7.5], comportamiento:["Tranquilo","Pacífico"], procedencia:"China", cardumen:false, esperanzaVida:[10,25]
         }
 
         {
-            nombre:"", especie:"", tamañoAdulto:[], razas: [""],
-            reproducción: "", tamañoMinimoAcuario: , colores:[""],
-            temperaturaOptima:[], alimentación:"", tipoAgua:[""] , pH:[], comportamiento:[""],
-            procedencia:[""], esperanzaVida:[]
-        }
+            nombre:"Pez payaso",especie:"Amphiprion ocellaris",tamaño:"8,9,10,11",reproducción:"Ovípara",volumenAcuario:80,temperatura:[24,25,26,27],alimentación:"Omnívora",
+            agua:["Salada","Cálida"], pH:[8,8.4], comportamiento:["Pacífico","Tranquilo"], procedencia:["Australia","Malasia","Tailandia"], cardumen:false, esperanzaVida: 10
+        },
 
         {
-            nombre:"", especie:"", tamañoAdulto:[], razas: [""],
-            reproducción: "", tamañoMinimoAcuario: , colores:[""],
-            temperaturaOptima:[], alimentación:"", tipoAgua:[""] , pH:[], comportamiento:[""],
-            procedencia:[""], esperanzaVida:[]
-        }
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
 
+        {
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
 
+        {
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
 
+        {
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
+
+        {
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
+
+        {
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
+
+        {
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
+
+        {
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
+
+        {
+            nombre:"",especie:"",tamaño:"",reproducción:"",volumenAcuario:,temperatura:,alimentación:"",
+            agua:, pH:, comportamiento:, procedencia:, cardumen:, esperanzaVida:
+        },
     ]    
 
 )
